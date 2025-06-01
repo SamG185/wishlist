2df457ce79044bcc12a2e54d4bf5e9768b378531
Sam Goodberry <samgoodberry@gmail.com>
2025-06-01 12:50:38 -0400

    Add basic error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,18 +12,24 @@ import { WishService } from './wish.service';
 })
 export class AppComponent implements OnInit {
   filter: any;
-  items! : WishItem[];
+  items : WishItem[] = [];
   constructor(events: EventService, private wishService: WishService){ 
     events.listen('removeWish', (wish: any) => {
       this.items.splice(this.items.indexOf(wish),1)
     })
   }
   ngOnInit(): void {
-    this.filter = '0';
-    this.wishService.getWishes().subscribe((data: any) => {
+    this.wishService.getWishes().subscribe(
+      (data: any) => {
       this.items=data;
-    });
+    },
+    (error: any) => {
+      alert(error.message);
+    }
+  
+  );
   }
+
   
 }
 
--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { WishItem } from 'src/shared/models/wishItem';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -25,12 +27,24 @@ export class WishService {
         format: 'json'
       }
     })
-    return this.http.get('assets/wishes.json', options);
+    return this.http.get('assets/wishes.json', options)
+    .pipe(catchError(this.handleError));
   }
 
   private addWish(wish :WishItem){
     let options = this.getStandardOptions();
     options.headers = options.headers.set('Authorization', 'auth-value');
     this.http.post('assets/wishes.json', wish, options)
+  }
+
+  private handleError(error: HttpErrorResponse){
+    if(error.status == 0){
+      console.error('There is an issue with the client or network: ', error.error);
+    }
+    else{
+      console.error('Server Side error: ', error.error)
+    }
+
+    return throwError(() => new Error('Cannot retrieve wishes.'))
   }
 }
