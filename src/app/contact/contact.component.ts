83befe49433a4b77fc8f50298ce64d59cddd65c5
Sam Goodberry <samgoodberry@gmail.com>
2025-06-08 15:36:38 -0400

    Add dynamic custom validator

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { createInvalidDomainValidator } from './invalidEmailDomain';
+
+const invalidEmailDomain = createInvalidDomainValidator(['gmail.com', 'yahoo.com'])
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-
   contactForm = new FormGroup({
     senderNameControl: new FormControl('', Validators.required),
-    senderEmailControl: new FormControl('', [Validators.required, Validators.email]),
+    senderEmailControl: new FormControl('', [Validators.required, Validators.email, invalidEmailDomain]),
     senderMessageControl:  new FormControl('', [Validators.required, Validators.minLength(4)])
 
   })
-  
-
   submitForm(){
     console.log('hi')
   }
